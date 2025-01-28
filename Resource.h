b61a438bafae9584cccd3ac2d48a336b00cb92ae
Asurahan <logic@xaker.ru>
2025-01-28 20:05:45 +0400

    залил то что получилось

---
+++ Resource.h
@@ -0,0 +1,40 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется life2.rc
+
+#define IDS_APP_TITLE			113
+
+#define IDR_MAINFRAME			128
+#define IDD_LIFE2_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_LIFE2			107
+#define IDI_SMALL				108
+#define IDC_LIFE2			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+
+#define IDC_START        131
+#define IDC_STOP         132
+#define IDC_RANDOMIZE    133
+#define IDC_CLEAR        134 
+
+#define IDD_CHANGE_SIZE_DIALOG  112
+#define IDC_SIZE_EDIT           113
+#define IDC_CHANGE_SIZE_BUTTON  114
+#define IDC_CHANGE_SIZE      115 // или любое другое свободное значение
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
